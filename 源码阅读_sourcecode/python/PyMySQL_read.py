f6ab0b3a1ce1bdb294ebcf729d9a12495c5629bd
PegasusWang <291374108@qq.com>
2018-07-16 08:17:19 +0800

    PyMySQL 阅读

---
+++ 源码阅读_sourcecode/python/PyMySQL_read.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+"""
+代码阅读： 传 python 的 mysql driver，gevent patch 了 socket，需要用纯 python driver
+https://github.com/PyMySQL/PyMySQL
+
+目的：了解 python 的 mysql driver 如何工作的
+
+切入点：从示例 demo 看起，自顶向下看代码
+
+
+import pymysql.cursors
+
+# Connect to the database
+connection = pymysql.connect(host='localhost',
+                             user='user',
+                             password='passwd',
+                             db='db',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+
+try:
+    with connection.cursor() as cursor:
+        # Create a new record
+        sql = "INSERT INTO `users` (`email`, `password`) VALUES (%s, %s)"
+        cursor.execute(sql, ('webmaster@python.org', 'very-secret'))
+
+    # connection is not autocommit by default. So you must commit to save
+    # your changes.
+    connection.commit()
+
+    with connection.cursor() as cursor:
+        # Read a single record
+        sql = "SELECT `id`, `password` FROM `users` WHERE `email`=%s"
+        cursor.execute(sql, ('webmaster@python.org',))
+        result = cursor.fetchone()
+        print(result)
+finally:
+    connection.close()
+"""
