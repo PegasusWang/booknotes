20e77e7a5eab859e0a24276ccffcc87e38d80c27
wangningning <wangningning@megvii.com>
2019-04-26 21:45:29 +0800

    go rest api demo

--- docs/golang/go-build-web-applications/go.mod
+++ docs/golang/go-build-web-applications/go.mod
@@ -3,6 +3,11 @@ module test-go-web
 go 1.12
 
 require (
-	github.com/go-sql-driver/mysql v1.4.1 // indirect
-	github.com/gorilla/mux v1.7.1 // indirect
+	github.com/go-sql-driver/mysql v1.4.1
+	github.com/gorilla/mux v1.7.1
+	golang.org/x/crypto v0.0.0-20190424203555-c05e17bb3b2d // indirect
+	golang.org/x/net v0.0.0-20190424112056-4829fb13d2c6 // indirect
+	golang.org/x/sys v0.0.0-20190425145619-16072639606e // indirect
+	golang.org/x/text v0.3.2 // indirect
+	golang.org/x/tools v0.0.0-20190425222832-ad9eeb80039a // indirect
 )
--- docs/golang/go-build-web-applications/main.go
+++ docs/golang/go-build-web-applications/main.go
@@ -1,6 +1,7 @@
 package main
 
 /*
+export GO111MODULE=on
 CREATE TABLE `pages` (
 	`id` int(11) unsigned NOT NULL AUTO_INCREMENT,
 	`page_guid` varchar(256) NOT NULL DEFAULT '',
@@ -13,16 +14,32 @@ CREATE TABLE `pages` (
 
 INSERT INTO `pages` (`id`, `page_guid`, `page_title`, `page_content`, `page_date`) VALUES (NULL, 'hello-world', 'Hello, World', 'I\'m so glad you found this page!  It\'s been sitting patiently on the Internet for some time, just waiting for a visitor.', CURRENT_TIMESTAMP);
 INSERT INTO `pages` (`id`, `page_guid`, `page_title`, `page_content`, `page_date`) VALUES (3, 'a-new-blog', 'A New Blog', 'I hope you enjoyed the last blog!  Well brace yourself, because my latest blog is even <i>better</i> than the last!', '2015-04-29 02:16:19');
+INSERT INTO `pages` (`id`, `page_guid`, `page_title`, `page_content`, `page_date`) VALUES (4, 'lorem-ipsum', 'Lorem Ipsum', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sem tortor, lobortis in posuere sit amet, ornare non eros. Pellentesque vel lorem sed nisl dapibus fringilla. In pretium...', '2015-05-06 04:09:45');
 
 // create user
 CREATE USER 'wnn'@'localhost';
 GRANT ALL PRIVILEGES ON test.* To 'wnn'@'localhost' IDENTIFIED BY 'wnnwnn';
 
+
+CREATE TABLE `comments` (
+`id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+`page_id` int(11) NOT NULL,
+`comment_guid` varchar(256) DEFAULT NULL,
+`comment_name` varchar(64) DEFAULT NULL,
+`comment_email` varchar(128) DEFAULT NULL,
+`comment_text` mediumtext,
+`comment_date` timestamp NULL DEFAULT NULL,
+PRIMARY KEY (`id`),
+KEY `page_id` (`page_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 */
 
 import (
 	"database/sql"
+	"encoding/json"
 	"html/template"
+	"strconv"
 
 	_ "github.com/go-sql-driver/mysql"
 
@@ -48,10 +65,39 @@ const (
 
 // Page def
 type Page struct {
+	ID         int
 	Title      string
 	RawContent string
 	Content    template.HTML
 	Date       string
+	Comments   []Comment
+	GUID       string
+	// Session    Session
+}
+
+// JSONResponse return type  struct
+type JSONResponse struct {
+	Fields map[string]string
+}
+
+// Comment def
+type Comment struct {
+	ID          int
+	Name        string
+	Email       string
+	CommentText string
+}
+
+// TruncateText def
+func (p Page) TruncateText() template.HTML {
+	chars := 0
+	for i := range p.Content {
+		chars++
+		if chars > 150 {
+			return p.Content[:i] + ` ...`
+		}
+	}
+	return p.Content
 }
 
 func pageHandler(w http.ResponseWriter, r *http.Request) {
@@ -90,13 +136,27 @@ func ServePageByGUID(w http.ResponseWriter, r *http.Request) {
 	pageGUID := vars["guid"]
 	thisPage := Page{}
 	fmt.Println(pageGUID)
-	err := database.QueryRow("select page_title,page_content,page_date from pages where page_guid=?", pageGUID).Scan(&thisPage.Title, &thisPage.Content, &thisPage.Date)
+	err := database.QueryRow(
+		"select id,page_title,page_content,page_date from pages where page_guid=?", pageGUID,
+	).Scan(&thisPage.ID, &thisPage.Title, &thisPage.Content, &thisPage.Date)
 	if err != nil {
 		log.Println("Couldn't get page:" + pageGUID)
 		http.Error(w, http.StatusText(404), http.StatusNotFound)
 		log.Println(err.Error)
 		return // 这里应该需要 return，书里没有
 	}
+
+	// query comments
+	comments, err := database.Query("SELECT id, comment_name as Name, comment_email, comment_text FROM comments WHERE page_id=?", thisPage.ID)
+	if err != nil {
+		log.Println(err)
+	}
+	for comments.Next() {
+		var comment Comment
+		comments.Scan(&comment.ID, &comment.Name, &comment.Email, &comment.CommentText)
+		thisPage.Comments = append(thisPage.Comments, comment)
+	}
+
 	t, _ := template.ParseFiles("templates/blog.html")
 	t.Execute(w, thisPage)
 }
@@ -109,19 +169,81 @@ func RedirIndex(w http.ResponseWriter, r *http.Request) {
 // ServeIndex def
 func ServeIndex(w http.ResponseWriter, r *http.Request) {
 	var Pages = []Page{}
-	pages, err := database.Query("select page_title,page_content,page_date from pages order by ? desc", "page_date")
+	pages, err := database.Query("select page_title,page_content,page_date,page_guid from pages order by ? desc", "page_date")
 	if err != nil {
 		fmt.Fprintln(w, err.Error)
 	}
-	defer pages.Close()
+	defer pages.Close() // need close
 	for pages.Next() {
 		thisPage := Page{}
-		pages.Scan(&thisPage.Title, &thisPage.RawContent, &thisPage.Date)
+		pages.Scan(&thisPage.Title, &thisPage.RawContent, &thisPage.Date, &thisPage.GUID)
 		thisPage.Content = template.HTML(thisPage.RawContent)
 		Pages = append(Pages, thisPage)
 	}
 	t, _ := template.ParseFiles("templates/index.html")
 	t.Execute(w, Pages)
+}
+
+// APIPage def
+func APIPage(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	pageGUID := vars["guid"]
+	thisPage := Page{}
+	fmt.Println(pageGUID)
+
+	err := database.QueryRow(
+		"SELECT page_title,page_content,page_date FROM pages WHERE page_guid=?", pageGUID,
+	).Scan(&thisPage.Title, &thisPage.RawContent, &thisPage.Date)
+	thisPage.Content = template.HTML(thisPage.RawContent)
+	if err != nil {
+		http.Error(w, http.StatusText(404), http.StatusNotFound)
+		log.Println(err)
+		return
+	}
+	APIOutput, err := json.Marshal(thisPage)
+	fmt.Println(APIOutput)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintln(w, thisPage)
+}
+
+// APICommentPost create comment
+func APICommentPost(w http.ResponseWriter, r *http.Request) {
+	var commentAdded bool
+	err := r.ParseForm()
+	if err != nil {
+		log.Println(err.Error)
+	}
+
+	name := r.FormValue("name")
+	email := r.FormValue("email")
+	comments := r.FormValue("comments")
+	pageID := r.FormValue("page_id")
+	res, err := database.Exec(
+		"insert into comments set page_id=?,comment_name=?,comment_email=?,comment_text=?", pageID, name, email, comments,
+	)
+	if err != nil {
+		log.Println(err.Error)
+	}
+
+	id, err := res.LastInsertId()
+	if err != nil {
+		commentAdded = false
+	} else {
+		commentAdded = true
+	}
+
+	commentAddedBool := strconv.FormatBool(commentAdded) // to string
+	var resp JSONResponse
+	resp.Fields = make(map[string]string)
+	resp.Fields["id"] = strconv.FormatInt(id, 10)
+	resp.Fields["added"] = commentAddedBool
+	jsonResp, _ := json.Marshal(resp.Fields)
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintln(w, jsonResp)
 }
 
 func main() {
@@ -134,11 +256,17 @@ func main() {
 	}
 	database = db
 
-	rtr := mux.NewRouter()
-	rtr.HandleFunc("/pages/{id:[0-9]+}", ServePage)
-	// rtr.HandleFunc("/pages/{guid:[a-z0-9A\\-]+}", ServePageByGUID)
-	// rtr.HandleFunc("/", RedirIndex)
-	// rtr.HandleFunc("/home", ServeIndex)
-	http.Handle("/", rtr)
+	routes := mux.NewRouter()
+	// rest api
+	routes.HandleFunc("/api/pages", APIPage).Methods("GET").Schemes("https")
+	routes.HandleFunc("/api/pages/{guid:[0-9a-zA\\-]+}", APIPage).Methods("GET").Schemes("https")
+	// comment rest api
+	routes.HandleFunc("/api/comments", APICommentPost).Methods("POST")
+
+	// routes.HandleFunc("/pages/{id:[0-9]+}", ServePage)
+	routes.HandleFunc("/pages/{guid:[a-z0-9A\\-]+}", ServePageByGUID)
+	routes.HandleFunc("/", RedirIndex)
+	routes.HandleFunc("/home", ServeIndex)
+	http.Handle("/", routes)
 	http.ListenAndServe(PORT, nil)
 }
