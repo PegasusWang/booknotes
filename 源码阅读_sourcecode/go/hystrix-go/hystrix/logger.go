cd92dc9dab24a328e0f35d772aeb4500edae076d
PegasusWang <291374108@qq.com>
2022-04-04 16:11:05 +0800

    Deployed fedcdf9b with MkDocs version: 1.0.4

--- 源码阅读_sourcecode/go/hystrix-go/hystrix/logger.go
+++
@@ -1,9 +0,0 @@
-package myhystrix
-
-type logger interface {
-	Printf(format string, items ...interface{})
-}
-
-type NoopLogger struct{}
-
-func (l NoopLogger) Printf(format string, items ...interface{}) {}
